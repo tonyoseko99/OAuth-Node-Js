165f844ddb419d2cfeb8ef54d1312649c412b84b
Tonny Oseko <tonnyoseko0@gmail.com>
2023-06-09 12:02:54 +0300

    configure passport js for authentication

--- passport-config.js
+++ passport-config.js
@@ -22,3 +22,13 @@ passport.use(
     }
   }
 );
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
