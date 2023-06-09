6389ac9f8935d5cdb62a91dc8a3c34a51df9c121
Tonny Oseko <tonnyoseko0@gmail.com>
2023-06-09 13:17:56 +0300

    create a signup route handler and hash password

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,20 @@ const router = express.Router();
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 
+const User = require("../models/User");
+
 router.post("/signup", async (req, res, next) => {
   // handle user signup
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/login", async (req, res, next) => {
