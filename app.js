b0cff249b7cf8ce4b8b5bd489d58836382e0c750
Tonny Oseko <tonnyoseko0@gmail.com>
2023-06-09 13:34:57 +0300

    create a middleware to authenticate the users

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 
+const authenticateToken = require("./middleware/authenticate");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/api/protected", authenticateToken);
 
 module.exports = app;
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
